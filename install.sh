d2b2be0ac8a82de3941196684e7f5602f0c10c39
Gerald Büchler <dev@genomast.de>
2020-05-02 00:22:09 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+apt update && apt dist-upgrade -y && apt autoremove -y && apt autoclean -y
+apt install -y ansible git hcloud-cli
+useradd -s /bin/bash -d /home/ansible -m -G users ansible
+mkdir -p /home/ansible/.ssh/
+chown ansible:ansible /home/ansible/.ssh/
+chmod 700 /home/ansible/.ssh/
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUk5Yk7aNThdmIPjC9oUYaix7dD51smtJhxRxOsjzG1 gerald@buechler.bayern" > /home/ansible/.ssh/authorized_keys
+chown ansible:ansible /home/ansible/.ssh/authorized_keys
+chmod 600 /home/ansible/.ssh/authorized_keys
+echo "source <(hcloud completion bash)" >> ~/.bashrc
+echo "source <(hcloud completion bash)" >> /home/ansible/.bashrc
+source <(hcloud completion bash)
+echo -e "************************************************************"
+echo -e "***"
+echo -e "*** Bitte den Hetzner Cloud API Token hinzufügen"
+echo -e "***"
+echo -e "*** ACHTUNG - Keine Anzeige von Zeichen"
+echo -e "***"
+echo -e "************************************************************"
+# hcloud context create my-controller
+su - ansible -c 'hcloud context create my-controller'
